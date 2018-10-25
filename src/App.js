9d963bb8e53a106c40f3e68e7332edf3f98505dc
AlejandraCP <alejandracp710@gmail.com>
2018-10-25 16:47:43 -0500

    dom event

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    name: 'Alejandra'
+  }
+
+  click(e) {
+    console.log(e.target);
+    
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            My name is {this.state.name}
           </p>
+          <button onClick={this.click}>Click me</button>
           <a
             className="App-link"
             href="https://reactjs.org"
